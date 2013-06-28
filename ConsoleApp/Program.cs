a29d8f9cbe0cb09ce7053389028664c353937400
Bright Twin <brighttwinsoftware@gmail.com>
2013-06-28 00:21:46 -0700

    Added wizard-created app, lib and PCL projects into the solution, so I can compare the CSPROJ files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ConsoleApp
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
---
+++ PortableClassLibrary/MyClass.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace PortableClassLibrary
+{
+	public class MyClass
+	{
+		public MyClass ()
+		{
+		}
+	}
+}
+
